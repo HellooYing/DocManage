a8e6ecb157efe5f028f80b424f0f7a9c2ff313a1
nekokira <2195751533@qq.COM>
2019-06-23 18:59:50 +0800

    create&write&read

--- src/main/java/com/NeuDocManage/service/DirService.java
+++ src/main/java/com/NeuDocManage/service/DirService.java
@@ -35,7 +35,7 @@ public class DirService {
 
     private static int findSubDir(int dirId,String dirName){
         //找一个目录的子目录
-        System.out.println(dirName);
+        //System.out.println(dirName);
         IndexNode nowInode = JSON.parseObject(readBlock(dirId).trim(), IndexNode.class);
         DirBlock nowDir = JSON.parseObject(readBlock(nowInode.getIndirectData()).trim(), DirBlock.class);
         if(nowDir.getSonDirId().size() == 0){
@@ -53,9 +53,19 @@ public class DirService {
     }
 
     public static int mkdir(String dirName) {
-        //创建一个目录,返回的是该目录的i节点号(创建失败返回BLOCKNUM - 1)
+        //创建一个目录,返回的是该目录的i节点号(创建失败返回 - 1)
+
+        //当前目录下已经有同名文件，返回-2
+        if(findSubDir(getCurDir().getId(),dirName) != -1){
+            return -2;
+        }
+
         int inodeNum = getIndexBlock(); //申请一个空闲i节点
         int dirBlockNum = getDataBlock(); //申请一个空闲数据块
+        System.out.println("hhh "+inodeNum+" "+dirBlockNum);
+        if(inodeNum == INODEBLOCKSTART+INODEBLOCKNUM-1 && dirBlockNum ==BLOCKNUM - 1 ){
+            return -1;
+        }
 
         //写入i节点信息
         IndexNode inode = new IndexNode();
@@ -79,7 +89,7 @@ public class DirService {
         dirBlock.setDirName(dirName);
         dirBlock.setIndexId(inodeNum);
 
-        System.out.println(getCurDir().getId());
+        //System.out.println(getCurDir().getId());
         dirBlock.setFaDirId(getCurDir().getId()); //父目录设置为当前目录，如果在初始化root目录时需要设置为空
 
         //新建目录以下两项都是空
@@ -92,9 +102,9 @@ public class DirService {
         DirBlock nowDir = JSON.parseObject(readBlock(nowInode.getIndirectData()).trim(), DirBlock.class);
         List<Integer> sonDir = nowDir.getSonDirId();
         sonDir.add(inodeNum);
-        System.out.println(JSON.toJSONString(sonDir));
+        //System.out.println(JSON.toJSONString(sonDir));
         nowDir.setSonDirId(sonDir);
-        System.out.println(getCurDir().getIndirectData());
+        //System.out.println(getCurDir().getIndirectData());
         overwriteBlock(getCurDir().getIndirectData(),JSON.toJSONString(nowDir));
 
 
@@ -102,18 +112,18 @@ public class DirService {
         dirBlock.setUsed(true);
 
         //写入磁盘
-        System.out.println(JSON.toJSONString(inode));
+        //System.out.println(JSON.toJSONString(inode));
         writeBlock(inodeNum, JSON.toJSONString(inode));
-        System.out.println(inodeNum+" "+readBlock(inodeNum));
+        //System.out.println(inodeNum+" "+readBlock(inodeNum));
         writeBlock(dirBlockNum, JSON.toJSONString(dirBlock));
-        System.out.println(dirBlockNum+" "+readBlock(dirBlockNum));
+       // System.out.println(dirBlockNum+" "+readBlock(dirBlockNum));
 
         return inodeNum;
     }
 
     private static Pair<Integer,String> cdAutomation(Pair<Integer,String> dirName){
         //目录自动机
-        System.out.println(dirName.getValue());
+        //System.out.println(dirName.getValue());
         if(dirName.getKey() == -1){
             return dirName; //出错了
         }else if(dirName.getValue().equals( "\\/")){
@@ -131,7 +141,7 @@ public class DirService {
              */
             int find = findSubDir(dirName.getKey(),subDir[0]);
             if(find != -1){
-                System.out.println("fuckyou");
+                //System.out.println("fuckyou");
                 if(subDir.length == 1){
 
                     //就剩一个目录了，直接返回
@@ -173,7 +183,7 @@ public class DirService {
             DirBlock nowDir = JSON.parseObject(readBlock(nowInode.getIndirectData()).trim(), DirBlock.class);
             cur = nowDir.getFaDirId();
         }
-        System.out.println(cur+" "+dirName);
+        //System.out.println(cur+" "+dirName);
         Pair<Integer, String> res = cdAutomation(new Pair<Integer, String>(cur,dirName));
         if(res.getKey() == -1){
             return -2; //没找到
@@ -191,12 +201,12 @@ public class DirService {
         //先读取所有目录输出
         for(Integer x : nowDir.getSonDirId()){
             nowInode = JSON.parseObject(readBlock(x).trim(), IndexNode.class);
-            result.add(nowInode.getFileName()+"\\/");
+            result.add(nowInode.getFileName()+"/");
         }
         //再读取所有子文件输出
         for(Integer x : nowDir.getSonDataId()){
             nowInode = JSON.parseObject(readBlock(x).trim(), IndexNode.class);
-            result.add(nowInode.getFileName()+"\\/");
+            result.add(nowInode.getFileName());
         }
         return result;
     }
@@ -210,7 +220,7 @@ public class DirService {
         //写入i节点信息
         IndexNode inode = new IndexNode();
         inode.setId(dataBlockNum);
-        inode.setType(1);
+        inode.setType(2);
 
         //mode是空的，表示只有root和自己能有全部权限
         inode.setUsed(true);
@@ -223,20 +233,21 @@ public class DirService {
         inode.setChangeTime(new Date());
 
         //inode.setOffset(dataBlockNum); ??
-        inode.setIndirectData(inodeNum); //指向自己，如果一个文件存不下，可能会扩增，indirect指向下一个
+        inode.setIndirectData(dataBlockNum); //指向所指的磁盘块
 
         //写入dataBlock信息
         DataBlock dataBlock = new DataBlock();
-        dataBlock.setNextDataId(BLOCKNUM - 1);;   //表示没有
+        dataBlock.setData("");
+        dataBlock.setNextDataId(BLOCKNUM - 1);   //表示没有
         dataBlock.setFaDirID(getCurDir().getId()); //设置父亲的目录
 
         //寻找父亲目录的子目录最后一个，添加到尾部
         IndexNode nowInode = JSON.parseObject(readBlock(getCurDir().getId()).trim(), IndexNode.class);
         DirBlock nowDir = JSON.parseObject(readBlock(nowInode.getIndirectData()).trim(), DirBlock.class);
         List<Integer> sons = nowDir.getSonDataId();
-        sons.add(dataBlockNum);
+        sons.add(inodeNum);
         nowDir.setSonDataId(sons);
-        writeBlock(getCurDir().getId(),JSON.toJSONString(nowDir));//写回
+        overwriteBlock(getCurDir().getIndirectData(),JSON.toJSONString(nowDir));//写回
 
         dataBlock.setUsed(true);
 
@@ -247,44 +258,103 @@ public class DirService {
         return inodeNum;
     }
 
-//    public static boolean writeFile(String fileName,String content){
-//        //往文件里头写东西
-//        //首先找到这个文件
-//        int fileId = -1;
-//        IndexNode nowInode = JSON.parseObject(readBlock(getCurDir().getId()).trim(), IndexNode.class);
-//        DirBlock nowDir = JSON.parseObject(readBlock(nowInode.getId()).trim(), DirBlock.class);
-//        for(Integer x : nowDir.getSonDataId()){
-//            nowInode = JSON.parseObject(readBlock(x).trim(), IndexNode.class);
-//            if(nowInode.getFileName().equals(fileName)){
-//                 fileId = nowInode.getId();
-//            }
-//        }
-//        if(fileId == -1){
-//            return false; //没找到
-//        }
-//        //设置content
-//        List<String> contents = new ArrayList<String>();
-//        int limit = 450;
-//        //首先判断String 长度，如果大于450，就分隔
-//        int beginIndex =  0;
-//        int endIndex = limit - 1;
-//        int length = content.length();
-//        while(length > limit){
-//            contents.add(content.substring(beginIndex,endIndex));
-//            length -= limit;
-//            beginIndex = endIndex+1;
-//            endIndex = endIndex + limit;
-//        }
-//        for (int i = 0; i < contents.size(); i++) {
-//            String nowContent = contents.get(i);
-//
-//            if(i == contents.size() - 1){
-//
-//            }else{
-//
-//            }
-//        }
-//    }
+    public static boolean writeFile(String fileName,String content){
+        //往文件里头写东西
+        //首先找到这个文件
+        int fileId = -1;
+        int headId = -1; //表示这个文件的头Id
+        IndexNode nowInode = JSON.parseObject(readBlock(getCurDir().getId()).trim(), IndexNode.class);
+        DirBlock nowDir = JSON.parseObject(readBlock(nowInode.getIndirectData()).trim(), DirBlock.class);
+        for(Integer x : nowDir.getSonDataId()){
+            nowInode = JSON.parseObject(readBlock(x).trim(), IndexNode.class);
+            if(nowInode.getFileName().equals(fileName)){
+                headId = nowInode.getIndirectData();
+                 //找到这个文件结尾的Id
+                 DataBlock nowData = JSON.parseObject(readBlock(headId).trim(), DataBlock.class);
+                 int nextId = nowData.getNextDataId();
+                 while(nextId !=BLOCKNUM - 1){
+                     nowInode = JSON.parseObject(readBlock(nextId).trim(), IndexNode.class);
+                     nowData =JSON.parseObject(readBlock(nowInode.getIndirectData()).trim(), DataBlock.class);
+                     nextId = nowData.getNextDataId();
+                 }
+                 fileId = nowInode.getIndirectData();
+            }
+        }
+        if(fileId == -1){
+            return false; //没找到
+        }
+        System.out.println(fileId);
+        DataBlock nowData = JSON.parseObject(readBlock(fileId).trim(), DataBlock.class);
+        //在文件结尾写，所以先把结尾连接上去
+        if(!nowData.getData().equals("")){
+            content = nowData.getData().concat(" "+content);
+        }
+        //设置content
+        List<String> contents = new ArrayList<String>();
+        int limit = 450;
+        //首先判断String 长度，如果大于450，就分隔
+        int beginIndex =  0;
+        int endIndex = limit - 1;
+        int length = content.length();
+        while(length > limit){
+            contents.add(content.substring(beginIndex,endIndex));
+            length -= limit;
+            beginIndex = endIndex+1;
+            endIndex = endIndex + limit;
+        }
+        contents.add(content.substring(beginIndex));
+
+        IndexNode inode = new IndexNode();
+        //先写末尾结点
+        int newData = -1;
+        int newInode = -1;
+        if(contents.size() > 1) {
+            newData = getDataBlock();
+            newInode = getIndexBlock();
+            nowData.setNextDataId(newInode);
+        }
+        nowData.setData(contents.get(0)); //替换数据
+        overwriteBlock(fileId,JSON.toJSONString(nowData));
+
+        for (int i = 1; i < contents.size(); i++) {
+            String nowContent = contents.get(i);
+            //首先写入其他信息
+            //写入i节点信息
+            inode.setId(newData);
+            inode.setType(2);
+
+            //mode是空的，表示只有root和自己能有全部权限
+            inode.setUsed(true);
+            inode.setSize(512); //一块数据512
+
+            inode.setFileName(fileName);
+            inode.setCreator(HostHolder.getUser().getUserName());
+
+            inode.setCreateTime(new Date());
+            inode.setChangeTime(new Date());
+
+            //inode.setOffset(dataBlockNum); ??
+            inode.setIndirectData(newData); //指向所指的磁盘块
+
+            //写新的数据区
+            DataBlock dataBlock = new DataBlock();
+            dataBlock.setNextDataId(BLOCKNUM - 1); //先表示成没有
+            dataBlock.setFaDirID(BLOCKNUM - 1); //由于是文件的一个部分，只有头结点设置父节点，不然在ls的时候会出现两个文件
+            dataBlock.setData(nowContent); //写入当前数据
+            dataBlock.setUsed(true);
+
+            writeBlock(newInode,JSON.toJSONString(inode));
+            writeBlock(newData,JSON.toJSONString(dataBlock));
+            if(i+1 !=  contents.size() - 1) {
+                //如果这个结点的下个结点不是最后一个结点
+                newData = getDataBlock();
+                newInode = getIndexBlock();
+                dataBlock.setNextDataId(newInode);
+                inode = new IndexNode();
+            }
+        }
+        return true;
+    }
 
     public static String readFile(String fileName){
         //读当前目录下的某个文件,返回文件内容
--- src/test/java/com/NeuDocManage/DirServiceTest.java
+++ src/test/java/com/NeuDocManage/DirServiceTest.java
@@ -35,8 +35,9 @@ public class DirServiceTest {
 
         initDisk();//初始化磁盘
         int id = mkdir("name");
+        //System.out.println(id);
         IndexNode inode= JSON.parseObject(readBlock(id).trim(),IndexNode.class);
-        System.out.println("shit"+readBlock(id));
+        //System.out.println("shit"+readBlock(id));
         HostHolder.setCurDir(inode); //设置当前目录是name
         int id2 = mkdir("fuck");
         IndexNode inode2= JSON.parseObject(readBlock(id2).trim(),IndexNode.class);
@@ -44,6 +45,44 @@ public class DirServiceTest {
         int id3 = mkdir("shit");
         HostHolder.setCurDir(inode); //设置当前目录是name
         assertEquals(id3,changeDir("./fuck/shit"));
+        releaseDisk();
+    }
+    /**
+     * 测试ls createFile
+     * @throws IOException
+     */
+    @Test
+    public void test3() throws IOException{
+        initDisk();//初始化磁盘
+        int id = mkdir("name");
+        //System.out.println(id);
+        IndexNode inode= JSON.parseObject(readBlock(id).trim(),IndexNode.class);
+        //System.out.println("shit"+readBlock(id));
+        HostHolder.setCurDir(inode); //设置当前目录是name
+        int id2 = mkdir("fuck");
+        IndexNode inode2= JSON.parseObject(readBlock(id2).trim(),IndexNode.class);
+        //HostHolder.setCurDir(inode2); //设置当前目录是fuck
+        int id3 = mkdir("shit");
+        HostHolder.setCurDir(inode); //设置当前目录是name
+        int id4 = createFile("hhh");
+        for(String s : listDir()){
+            System.out.println(s+" ");
+        }
+        releaseDisk();
+    }
+    /**
+     * 测试读写文件
+     * @throws IOException
+     */
+    @Test
+    public void test4()throws IOException{
+        initDisk();//初始化磁盘
+        int id = createFile("hhh");
+        System.out.println(id);
+        readFile("hhh");
+        writeFile("hhh","123456");
+        writeFile("hhh","12");
+        assertEquals("123456 12",readFile("hhh"));
         releaseDisk();
     }
 }
